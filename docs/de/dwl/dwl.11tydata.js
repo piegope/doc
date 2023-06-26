d72edd979d072c2894633819159c492f22714234
Christine Breault <cbreault@devolutions.net>
2023-06-26 11:00:33 -0400

    DWL Navogation Tree DE

---
+++ docs/de/dwl/dwl.11tydata.js
@@ -0,0 +1,15 @@
+module.exports = () => ({
+  layout: 'layouts/markdown.njk',
+  pagination: {
+    data: 'trees',
+    size: 1,
+    alias: 'tree'
+  },
+  permalink: '{{ (page.filePathStem.replace("dwl/", tree + "/dwl/") if tree !== "dwl" else page.filePathStem) | localized }}/',
+  trees: [
+    'hub',
+    'server',
+    'rdm/windows',
+    'rdm/mac',
+  ]
+});
