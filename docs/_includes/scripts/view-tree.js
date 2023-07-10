3fd95ffe85efdb9e188b4d66a3f5a7df081c4b05
Michael Proulx <mproulx@devolutions.net>
2023-07-10 14:30:21 -0400

    Switch ScreenHeight to ScrollbarHeight

--- docs/_includes/scripts/view-tree.js
+++ docs/_includes/scripts/view-tree.js
@@ -1,15 +1,14 @@
-
 const viewTree = {
-    init: function () {
-        const elements = document.getElementsByClassName('current');
-        const scrollbar = document.getElementsByClassName('navigation');
-        
-        if (elements.length > 0  && scrollbar.length > 0 ) {
-            scrollbar[0].scrollTop = elements[0].offsetTop - (screen.height / 2.5);
-        }
+  init: function () {
+    const elements = document.getElementsByClassName('current');
+    const scrollbar = document.getElementsByClassName('navigation');
+
+    if (elements.length > 0  && scrollbar.length > 0 ) {
+      scrollbar[0].scrollTop = elements[0].offsetTop - (scrollbar[0].offsetHeight / 2) + (elements[0].offsetHeight / 2);
     }
+  }
 };
 
 (function () {
-    viewTree.init();
+  viewTree.init();
 })();
