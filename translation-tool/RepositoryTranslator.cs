775e9f75824559fc77569de2c0711db95fb2b180
Dany Gauthier <dgauthier@devolutions.net>
2023-11-03 10:57:40 -0400

    Bug fixes and improvements
    *  L’outil va commencer par rechercher le dossier “content”. S’il ne le trouve pas, il recherchera le dossiers “docs”. S’il ne trouve ni l’un ni l’autre, ça donnera une erreur. Comme ça, ça va fonctionner avant et après les changements à Mike.
    * Un corrrectif a été appliqué pour l'entête en trop. Ça créait un nouveau bloc d’entête quand le fichier Markdown avait juste un bloc d’entête sans rien d’autre, et qu’aucun saut de ligne ne se trouvait après les derniers ---
    Ce cas spécial sera géré dorénavant
    * Dorénavant, pour title, description, la 1ère ligne de keywords, et aussi les autres lignes de keywords qui commencent par -, l’indentation sera préservée telle que trouvée dans le fichier source.
    * L'attribut NoDeepL est maintenant supporté
    * Le inline code est maintenant supporté (juste les variables qui sont prises en compte)
    * Les code blocs sont maintenant supportés (juste les variables qui sont prises en compte)

--- translation-tool/RepositoryTranslator.cs
+++ translation-tool/RepositoryTranslator.cs
@@ -43,9 +43,10 @@ public sealed partial class RepositoryTranslator
 
     public async Task Execute()
     {
-        string repositoryPath = this.GetRepositoryPath();
-        SourceLanguage sourceLanguage = this.GetSourceLanguage(repositoryPath);
-        TargetLanguage[] targetLanguages = await this.GetTargetLanguages(repositoryPath, sourceLanguage).ConfigureAwait(false);
+        string repositoryPath = GetRepositoryPath();
+        string contentDirectoryName = GetContentDirectoryName(repositoryPath);
+        SourceLanguage sourceLanguage = await this.GetSourceLanguage(repositoryPath, contentDirectoryName).ConfigureAwait(false);
+        TargetLanguage[] targetLanguages = await this.GetTargetLanguages(repositoryPath, contentDirectoryName, sourceLanguage).ConfigureAwait(false);
         MarkdownPipeline markdownPipeline = new MarkdownPipelineBuilder().UseAdvancedExtensions().Build();
         using Translator deeplTranslator = new(this.deeplAuthenticationKey, this.deeplTranslatorOptions);
         TranslatedFileCounter translatedFileCounter = new(this.maxTranslatedFileCount);
@@ -60,14 +61,14 @@ public sealed partial class RepositoryTranslator
         }
     }
 
-    private string GetRepositoryPath()
+    private static string GetRepositoryPath()
     {
         string directoryPath = Directory.GetCurrentDirectory();
         DirectoryInfo? directoryInfo = new(directoryPath);
         do
         {
             directoryPath = directoryInfo.FullName;
-            if (Directory.Exists(Path.Combine(directoryInfo.FullName, "docs")))
+            if (Directory.Exists(Path.Combine(directoryPath, "translation-tool")))
             {
                 return directoryPath;
             }
@@ -78,14 +79,22 @@ public sealed partial class RepositoryTranslator
         throw new InvalidOperationException("Current directory is not within the expected repository");
     }
 
-    private SourceLanguage GetSourceLanguage(string repositoryPath)
+    private static string GetContentDirectoryName(string repositoryPath) =>
+        GetContentDirectoryName(repositoryPath, "content") ??
+        GetContentDirectoryName(repositoryPath, "docs") ??
+        throw new InvalidOperationException("Content directory has not been found");
+
+    private static string? GetContentDirectoryName(string repositoryPath, string contentDirectoryName) =>
+        Directory.Exists(Path.Combine(repositoryPath, contentDirectoryName)) ? contentDirectoryName : null;
+
+    private async Task<SourceLanguage> GetSourceLanguage(string repositoryPath, string contentDirectoryName)
     {
-        SourceLanguage sourceLanguage = new(this.sourceLanguageCode, Path.Combine(repositoryPath, "docs", this.sourceLanguageCode));
-        sourceLanguage.Files = GetSourceFiles(repositoryPath, sourceLanguage);
+        SourceLanguage sourceLanguage = new(this.sourceLanguageCode, Path.Combine(repositoryPath, contentDirectoryName, this.sourceLanguageCode));
+        sourceLanguage.Files = await GetSourceFiles(repositoryPath, sourceLanguage).ConfigureAwait(false);
         return sourceLanguage;
     }
 
-    private static SourceFile[] GetSourceFiles(string repositoryPath, SourceLanguage sourceLanguage)
+    private static async Task<SourceFile[]> GetSourceFiles(string repositoryPath, SourceLanguage sourceLanguage)
     {
         string[] sourceFilePaths = Directory.GetFiles(sourceLanguage.DirectoryPath, "*.md", SearchOption.AllDirectories);
 
@@ -96,7 +105,7 @@ public sealed partial class RepositoryTranslator
         foreach (string sourceFilePath in sourceFilePaths)
         {
             string? latestCommitFromGit = GetLatestCommitFromGit(repositoryPath, latestCommit, sourceFilePath);
-            if (latestCommitFromGit != null)
+            if (latestCommitFromGit != null && !(await IsSourceFileExcluded(sourceFilePath).ConfigureAwait(false)))
             {
                 result.Add(new SourceFile(sourceLanguage, sourceFilePath, latestCommitFromGit));
             }
@@ -152,12 +161,21 @@ public sealed partial class RepositoryTranslator
         return commit.Sha;
     }
 
-    private async Task<TargetLanguage[]> GetTargetLanguages(string repositoryPath, SourceLanguage sourceLanguage)
+    private static async Task<bool> IsSourceFileExcluded(string filePath)
+    {
+        string content = await File.ReadAllTextAsync(filePath).ConfigureAwait(false);
+        return NoDeepLRegex().IsMatch(content);
+    }
+
+    [GeneratedRegex(@"^ *noDeepL:", RegexOptions.CultureInvariant | RegexOptions.IgnoreCase | RegexOptions.Multiline)]
+    private static partial Regex NoDeepLRegex();
+
+    private async Task<TargetLanguage[]> GetTargetLanguages(string repositoryPath, string contentDirectoryName, SourceLanguage sourceLanguage)
     {
         List<TargetLanguage> result = new(this.targetLanguageCodes.Length);
         foreach (string targetLanguageCode in this.targetLanguageCodes)
         {
-            string directoryPath = Path.Combine(repositoryPath, "docs", $"{targetLanguageCode}.generated");
+            string directoryPath = Path.Combine(repositoryPath, contentDirectoryName, $"{targetLanguageCode}.generated");
             string? glossaryID = this.options.GetGlossaryID(sourceLanguage.Code, targetLanguageCode)?.Trim().NullIfEmpty();
             string cacheFilePath = Path.Combine(repositoryPath, ".translation-tool", glossaryID != null ?
                 $"{sourceLanguage.Code}-{targetLanguageCode}-{glossaryID}-cache.json" : $"{sourceLanguage.Code}-{targetLanguageCode}-cache.json");
