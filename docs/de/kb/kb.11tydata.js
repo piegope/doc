569a19708452e6ec7b41058c24fd032a9078f1be
Michael Proulx <proulx.mick@gmail.com>
2023-12-20 16:11:01 -0500

    Replace algolia auto-complet for devo-search

--- docs/de/kb/kb.11tydata.js
+++ docs/de/kb/kb.11tydata.js
@@ -2,9 +2,6 @@ module.exports = function() {
   return {
     layout: 'layouts/markdown.njk',
     trees: ['kb'],
-    tree: 'kb',
-    searchTags: [
-      { 'label': 'Knowledge Base', 'facet': 'doc' }
-    ]
+    tree: 'kb'
   }
 }
--- docs/en/hub/hub.11tydata.js
+++ docs/en/hub/hub.11tydata.js
@@ -2,9 +2,6 @@ module.exports = function() {
   return {
     layout: 'layouts/markdown.njk',
     trees: ['hub'],
-    tree: 'hub',
-    searchTags: [
-      { 'label': 'Devolutions Hub', 'facet': 'doc' }
-    ]
+    tree: 'hub'
   }
 }
