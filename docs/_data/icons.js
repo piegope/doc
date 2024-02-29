eb39c6d814a1ffc220a08a66fd45e40ceb6c5676
Jean-Sébastien Simard <jssimard@devolutions.net>
2024-02-29 15:32:05 -0500

    icons.js - image link

--- docs/_data/icons.js
+++ docs/_data/icons.js
@@ -1,7 +1,7 @@
 const EleventyFetch = require("@11ty/eleventy-fetch");
 
 module.exports = async function() {
-  let url = "https://cdnweb.devolutions.net/devolutions-icons/3.4.1/inline-svg/devolutions-icons.json";
+  let url = "https://cdn.devolutions.net/devolutions-icons/3.4.1/inline-svg/devolutions-icons.json";
 
   return EleventyFetch(url, {
     duration: "*",
