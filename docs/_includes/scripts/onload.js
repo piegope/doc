7904a003766e0e883910c3b65ff9b15f47634f76
Olivier Beaulieu <obeaulieu@devolutions.net>
2024-03-29 09:27:23 -0400

    Ajout changement d'icone dark/light

--- docs/_includes/scripts/onload.js
+++ docs/_includes/scripts/onload.js
@@ -2,8 +2,13 @@ document.addEventListener('DOMContentLoaded', function() {
     
     if (localStorage.getItem('theme') === "dark-theme") {
         document.body.classList.add('dark-theme');
+        document.getElementById('dark-icon').style.display = 'none';
+        document.getElementById('light-icon').style.display = 'block';
+       
     } else {
         document.body.classList.remove('dark-theme');
+        document.getElementById('dark-icon').style.display = 'block';
+        document.getElementById('light-icon').style.display = 'none';
     }
     dwdLogosDarkTheme.init();
 
--- docs/_includes/scripts/theme-picker.js
+++ docs/_includes/scripts/theme-picker.js
@@ -2,8 +2,12 @@ document.getElementById('theme-button').addEventListener('click', () => {
     document.body.classList.toggle('dark-theme');
     if (document.body.classList.contains('dark-theme')) {
       localStorage.setItem('theme', 'dark-theme');
+      document.getElementById('dark-icon').style.display = 'none';
+      document.getElementById('light-icon').style.display = 'block';
     } else {
       localStorage.setItem('theme', '');
+      document.getElementById('dark-icon').style.display = 'block';
+      document.getElementById('light-icon').style.display = 'none';
     }
     
     dwdLogosDarkTheme.init();
