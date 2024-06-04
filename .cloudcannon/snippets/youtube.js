3826142bf516d6d225385dd7404f369567d0286d
Michael Proulx <proulx.mick@gmail.com>
2024-06-04 15:53:02 -0400

    Add icon_color to youtube snippet

--- .cloudcannon/snippets/youtube.js
+++ .cloudcannon/snippets/youtube.js
@@ -10,6 +10,7 @@ module.exports = {
         }
       ],
       icon: "smart_display",
+      icon_color: "#ff0000",
       image: [
         {
           template: "https://i.ytimg.com/vi_webp/{id}/maxresdefault.webp"
